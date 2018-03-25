b2695819d210a1a2a5c407e213418ce597498999
Fede <fedegonzalez_84@hotmail.com>
2018-03-25 16:20:06 -0300

    Se ha creado el esqueleto de las rutas con algunos datos de prueba

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 'use strict'
-
+//Requires
 const express = require('express');
 const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Middlewares
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//Routes
+app.get('/api/product', (req, res) => {
+  res.status(200).send({ products: []});
+});
+
+app.get('/api/product/:productID', (req, res) => {
+ 
+});
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body);
+  res.status(200).send({message: 'El producto se ha recibido'});
+});
+
+app.put('/api/product/:productId', (req, res) => {
+
+});
+
+app.delete('/api/product/:productId',(req, res) =>{
+
+});
+
 
 app.listen(port, () => {
   console.log(`API REST corriendo en http://localhost: ${port}`); 
