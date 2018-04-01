1fcf37e24c508b1c0a7c531c38f333493662ab42
Fede <fedegonzalez_84@hotmail.com>
2018-04-01 17:15:50 -0300

    Finalizada la API, se le agrego una vista para que el usuario tenga unos campos para loguearse

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //Requires
 const express = require('express');
 const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
 const app = express();
 const api = require('./routes');
 //----------------------------------------------------------------------------------------------------
@@ -9,6 +10,18 @@ const api = require('./routes');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/api', api);
+app.use('/login', (req, res) => {
+  res.render('login');
+});
+app.use('/', (req, res) => {
+  res.render('products');
+});
+
+app.engine('.hbs', hbs({
+  defaultLayout: 'default',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 //----------------------------------------------------------------------------------------------------
 //Exports
 module.exports = app;
